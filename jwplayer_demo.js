693e6fca7ed99c8a9314f35817537d1addf9cab9
Karan Kankaria <51224137+karankankaria@users.noreply.github.com>
2020-09-13 18:22:31 +0530

    Update jwplayer_demo.js

--- jwplayer_demo.js
+++ jwplayer_demo.js
@@ -1,4 +1,4 @@
 // Set up the player
 const playerInstance = jwplayer('player').setup({
-  playlist: 'https://raw.githubusercontent.com/karankankaria/JWPlayer/master/v2/media/8L4m9FJB'
+  playlist: 'https://raw.githubusercontent.com/karankankaria/JWPlayer/master/v2/media/demo'
 });
